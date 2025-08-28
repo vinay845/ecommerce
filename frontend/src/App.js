ccaa3592631e13a285e796395f07e8ca678d2592
vinay845 <vinaykahar978@gmail.com>
2025-08-28 18:57:30 +0530

    HomeScreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {Container} from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App =() => {
   return (
     <>
     <Header/>
     <main className='py-3'>
-      <Container><h1>Welcome to VinnuShop</h1></Container>
-      
+      <Container>
+        <HomeScreen/>
+      </Container>      
     </main> 
     <Footer/>     
     </>
